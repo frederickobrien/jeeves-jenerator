00ce6dcfa28f774eceff43abe67ad280924fd48e
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2021-11-17 16:57:40 +0000

    Update dilemmasList.js

--- lists/dilemmasList.js
+++ lists/dilemmasList.js
@@ -3,5 +3,8 @@ export const dilemmas = [
     'his pal Bingo is hopelessly in love with a woman all wrong for him',
     'Aunt Agatha has frozen his allowance again',
     'a chap at the club has nominated him to run for parliament',
-    'a communist down the road is rather keen on removing his eyeballs'
+    'a communist down the road is keen on removing his eyeballs',
+    'the fellow downstairs has vowed to remove his spine',
+    'has been named godfather of some wretched brat and is expected to babysit',
+    'Aunt Agatha has insisted that he take etiquette lessons'
   ];
