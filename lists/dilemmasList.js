9ec562225e7a0672142c7bab390753b139540e40
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2021-11-17 17:04:24 +0000

    Update dilemmasList.js

--- lists/dilemmasList.js
+++ lists/dilemmasList.js
@@ -5,6 +5,6 @@ export const dilemmas = [
     'a chap at the club has nominated him to run for parliament',
     'a communist down the road is keen on removing his eyeballs',
     'the fellow downstairs has vowed to remove his spine',
-    'he has been named godfather of some wretched brat and is expected to babysit',
+    'he has been named godfather to some wretched brat and is expected to babysit',
     'Aunt Agatha has insisted that he take etiquette lessons'
   ];
