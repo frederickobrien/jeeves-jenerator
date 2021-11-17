3e93080bf62ce16becb9e38579d9027b51ed3e13
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2021-11-17 16:53:56 +0000

    Update solutionsList.js

--- lists/solutionsList.js
+++ lists/solutionsList.js
@@ -2,8 +2,8 @@ export const solutions = [
     'passing the whole thing off as a practical joke',
     'fleeing with Wooster to Australia',
     'fleeing with Wooster to New York',
-    'selling the story to a renowned author with writers block',
+    'selling the story to the press and using the money to pay off those affected',
     'finding a doppelgänger to take the fall',
-    'calling in a few favours from Buckingham Palace',
-    'staging a failed a coup to distract from Woosters misteps'
+    'calling in a personal favour from Buckingham Palace',
+    'masterminding a coup to distract from Wooster’s missteps'
   ];
