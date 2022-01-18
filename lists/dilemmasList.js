425b7070189eaca0c8ed26dffa123daf928c7652
Frederick O'Brien <frederik.obrien@gmail.com>
2022-01-18 19:34:23 +0000

    Build fix

--- lists/dilemmasList.js
+++ lists/dilemmasList.js
@@ -7,7 +7,7 @@ export const dilemmas = [
     'the fellow downstairs has vowed to remove his spine',
     'he has been named godfather to some wretched brat and is expected to babysit',
     'Aunt Agatha has insisted that he take etiquette lessons',
-    'he has been asked to deliver a lecture on high society dance at his alma mater - Magdalen College, Oxford'
-    'those blots at the tax office want him to part with a hefty fraction of a his hard-earned allowance',
+    'he has been asked to deliver a lecture on high society dance at his alma mater - Magdalen College, Oxford',
+    'those blots at the tax office want him to part with a hefty fraction of his hard-earned allowance',
     'his pal Biffy has roped him into a double date with one of his ex-fiances'
   ];
