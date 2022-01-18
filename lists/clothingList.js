ad906605f8ebabf013ea3aeb63fcd4e212feb897
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2022-01-18 19:07:47 +0000

    Add to clothing list

--- lists/clothingList.js
+++ lists/clothingList.js
@@ -7,5 +7,11 @@ export const clothing = [
     'denim socks',
     'a polka dot blazer',
     'a pink-tinted monocle',
-    'a plaid muumuu'
+    'a plaid muumuu',
+    'maroon pyjamas',
+    'a leather jacket',
+    'a beige beret',
+    'a pink and yellow tie',
+    'safari shorts',
+    'green chinos'
 ];
