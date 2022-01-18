aa9d8fdfbd2cc3f320fd6f276a5f78a1745cf369
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2022-01-18 19:22:15 +0000

    Add to mishaps list

--- lists/mishapsList.js
+++ lists/mishapsList.js
@@ -4,5 +4,7 @@ export const mishaps = [
     'becoming engaged to three different women during one tea party',
     'getting himself placed on the international most wanted list',
     'burning down a wing of the Natural History Museum',
-    'unwittingly enlisting in the army'
+    'unwittingly enlisting in the army',
+    'spilling laxative into the soup at a royal dinner party',
+    'losing most of his fortune in a game of baccarat against some blasted secret agent type'
   ];
